cbe69e55345043abb2d7b10601344c9ef79e0a88
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-01-28 15:53:33 +0300

    listing 2.2 update

--- ch02/listing_2.2/run.sh
+++ ch02/listing_2.2/run.sh
@@ -18,10 +18,12 @@ protoc \
 
 go mod tidy
 
+echo "####START####"
 echo "Running server..."
 nohup go run server/server.go &
 
 echo "Running client..."
 go run client/client.go
+echo "####END####"
 
 
