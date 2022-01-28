83b49bdcf8500b5df6564ecbbbf59cc902f4dfdf
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-01-28 16:37:24 +0300

    listing 3.1 added

---
+++ ch03/listing_3.1/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Generating Go source code for order.proto"
+mkdir -p golang
+protoc \
+    -I ./proto
+    --go_out=./golang \
+    --go_opt=paths=source_relative \
+    --go-grpc_out=./golang \
+    --go-grpc_opt=paths=source_relative \
+    ./proto/order.proto
+
