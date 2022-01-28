52692eeda3aad6496a8e9b142f9733eb1b56286b
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-01-28 19:11:34 +0300

    listing find refined

--- ch03/listing_3.2/run.sh
+++ ch03/listing_3.2/run.sh
@@ -17,6 +17,7 @@ cd ../../
 git config --global user.email "huseyinbabal88@gmail.com"
 git config --global user.name "Huseyin BABAL"
 git add . && git commit -am "proto update" || true
+git push -u origin HEAD
 git tag -fa ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION} \
   -m "ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}"
 git push origin refs/tags/ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
