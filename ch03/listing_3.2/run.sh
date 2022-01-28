827d66071cc3f6ff01745caeaaccd3940b5115d0
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-01-28 19:04:35 +0300

    listing find refined

--- ch03/listing_3.2/run.sh
+++ ch03/listing_3.2/run.sh
@@ -11,12 +11,12 @@ protoc --go_out=./golang \
 
 cd golang/${SERVICE_NAME}
 go mod init \
-  github.com/huseyinbabal/microservices-proto/golang/${SERVICE_NAME} || true
+  github.com/huseyinbabal/grpc-microservices-in-go/ch03/listing_3.2/golang/${SERVICE_NAME} || true
 go mod tidy
 cd ../../
 git config --global user.email "huseyinbabal88@gmail.com"
 git config --global user.name "Huseyin BABAL"
 git add . && git commit -am "proto update" || true
-git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
-  -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+git tag -fa ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION} \
+  -m "ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
